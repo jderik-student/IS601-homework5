71d226d87b8058a9f953cfaa69c8fead2875b359
jad553 <jad553@drexel.edu>
2024-06-13 14:28:58 -0400

    REPL commands are now dynamically loaded in as plugins

--- app/__init__.py
+++ app/__init__.py
@@ -4,16 +4,10 @@
     Defines the Application which is a REPL defined to be an interactive calculator
 '''
 
+import pkgutil
+import importlib
 from app.commands import CommandHandler
-from app.commands.add import AddCommand
-from app.commands.subtract import SubtractCommand
-from app.commands.multiply import MultiplyCommand
-from app.commands.divide import DivideCommand
-from app.commands.getHistory import GetHistoryCommand
-from app.commands.clearHistory import ClearHistoryCommand
-from app.commands.menu import MenuCommand
-from app.commands.exit import ExitCommand
-
+from app.commands import Command
 
 class App:
     '''
@@ -22,20 +16,32 @@ class App:
     def __init__(self): # Constructor
         self.command_handler = CommandHandler()
 
+    def load_plugins(self):
+        '''
+            # Dynamically load all plugins in the plugins directory
+        '''
+        plugins_package = 'app.plugins'
+        for _, plugin_name, is_pkg in pkgutil.iter_modules([plugins_package.replace('.', '/')]):
+            if is_pkg:
+                plugin_module = importlib.import_module(f'{plugins_package}.{plugin_name}')
+                for item_name in dir(plugin_module):
+                    item = getattr(plugin_module, item_name)
+                    try:
+                        if issubclass(item, (Command)) and item is not Command:  # Added extra condition as it was registering the command twice
+                            if plugin_name == "menu":
+                                self.command_handler.register_command(plugin_name, item(self.command_handler))
+                            else:
+                                self.command_handler.register_command(plugin_name, item())
+                    except TypeError:
+                        continue  # If item is not a class or unrelated class, just ignore
+
     def start(self):
         '''
             Registers all the commands to be used by the user and starts the REPL
             Exits the app when the user types 'exit'
         '''
         # Register commands here
-        self.command_handler.register_command("add", AddCommand())
-        self.command_handler.register_command("subtract", SubtractCommand())
-        self.command_handler.register_command("multiply", MultiplyCommand())
-        self.command_handler.register_command("divide", DivideCommand())
-        self.command_handler.register_command("exit", ExitCommand())
-        self.command_handler.register_command("getHistory", GetHistoryCommand())
-        self.command_handler.register_command("clearHistory", ClearHistoryCommand())
-        self.command_handler.register_command("menu", MenuCommand(self.command_handler))
+        self.load_plugins()
 
         self.command_handler.list_commands()
         print("Type 'exit' to exit.")
