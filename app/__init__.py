faf7b0702d6686732ebcc767b13a03d50f2e88b1
jad553 <jad553@drexel.edu>
2024-06-13 11:17:06 -0400

    Created REPL app

---
+++ app/__init__.py
@@ -0,0 +1,40 @@
+# pylint: disable=unnecessary-dunder-call, invalid-name, unnecessary-pass
+
+'''
+    Defines the Application which is a REPL defined to be an interactive calculator
+'''
+
+from app.commands import CommandHandler
+from app.commands.add import AddCommand
+from app.commands.subtract import SubtractCommand
+from app.commands.multiply import MultiplyCommand
+from app.commands.divide import DivideCommand
+from app.commands.getHistory import GetHistoryCommand
+from app.commands.clearHistory import ClearHistoryCommand
+from app.commands.exit import ExitCommand
+
+
+class App:
+    '''
+        The Application is a REPL defined to be an interactive calculator
+    '''
+    def __init__(self): # Constructor
+        self.command_handler = CommandHandler()
+
+    def start(self):
+        '''
+            Registers all the commands to be used by the user and starts the REPL
+            Exits the app when the user types 'exit'
+        '''
+        # Register commands here
+        self.command_handler.register_command("add", AddCommand())
+        self.command_handler.register_command("subtract", SubtractCommand())
+        self.command_handler.register_command("multiply", MultiplyCommand())
+        self.command_handler.register_command("divide", DivideCommand())
+        self.command_handler.register_command("exit", ExitCommand())
+        self.command_handler.register_command("getHistory", GetHistoryCommand())
+        self.command_handler.register_command("clearHistory", ClearHistoryCommand())
+
+        print("Type 'exit' to exit.")
+        while True:  #REPL Read, Evaluate, Print, Loop
+            self.command_handler.execute_command(input(">>> ").split())
